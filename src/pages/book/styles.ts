227c67d6b3f0e719a6c9270485c3e6e708b2145e
Ramazza <01ramazza@gmail.com>
2026-03-16 12:26:34 -0300

    Versioning untracked files

--- src/pages/book/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const PDF = styled.iframe`
-  height: 100vh;
-  width: 100vw;
-`;
--- src/pages/home/styles.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const HomeWrapper = styled.div`
-    height: 100vh;
-`;
